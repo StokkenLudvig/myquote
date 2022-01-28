3aefaae25bca429c58d9c59198ede516f7fe4596
StokkenLudvig <lg_stokke@hotmail.com>
2022-01-28 20:46:59 +0100

    move file

---
+++ myquote.go
@@ -0,0 +1,21 @@
+package myquote
+
+import (
+	"rsc.io/quote"
+)
+
+func MyGlass() string {
+	return quote.Glass()
+}
+
+func MyGo() string {
+	return quote.Go()
+}
+
+func MyHello() string {
+	return quote.Hello()
+}
+
+func MyOpt() string {
+	return quote.Opt()
+}
